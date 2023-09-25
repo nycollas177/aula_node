a7186dac93752a52f979013cfb570ad8bb81631d
EA - Nycollas Paulino Da Silva <nycollas_silva@educadventista.org>
2023-09-25 15:13:56 -0300

    Aula 1

--- 2_EXPRESS/4_nodemon/index.js
+++ 2_EXPRESS/4_nodemon/index.js
@@ -1,15 +1,12 @@
-const express = require("express")
+const express = require('express')
 const app = express()
 
-const path = require("path")
+const path = require('path')
 
-const caminho = path.join(__dirname, "templates")
+const basePath = path.join(__dirname, 'templates')
 
-app.get("/", (requisicao, resposta) => {
-    resposta.sendFile(`${caminho}/index.html`)
+app.get('/', (req, res) => {
+    res.sendFile(`${basePath}/index.html`)
 })
 
-
-app.listen(3000, () => {
-    console.log("servidor")
-})
+app.listen(3000)
